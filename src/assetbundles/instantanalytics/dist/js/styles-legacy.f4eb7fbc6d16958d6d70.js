dd5aceeaded1f617e9d087f773f151d3653a9f18
Andrew Welch <andrew@nystudio107.com>
2018-10-04 13:50:45 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/assetbundles/instantanalytics/dist/js/styles-legacy.f4eb7fbc6d16958d6d70.js
@@ -0,0 +1,11 @@
+/*!
+ * @project        nystudio107
+ * @name           styles-legacy.f4eb7fbc6d16958d6d70.js
+ * @author         andrew@nystudio107.com
+ * @build          Thu, Oct 4, 2018 5:42 PM ET
+ * @release        fa3408cec54e50c0f45305179166221dc7914e5c [develop]
+ * @copyright      Copyright (c) 2018 nystudio107
+ *
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var s=function(){var t=this.$createElement;return(this._self._c||t)("main")};s._withStripped=!0;var i=e(4),o=e(9),u=e.n(o);i.default.use(u.a);var a={mounted:function(){var t=this;this.$confetti.start(),setTimeout(function(){t.$confetti.stop()},5e3)},methods:{}},c=e(10),r=Object(c.a)(a,s,[],!1,null,null,null);r.options.__file="src/assetbundles/instantanalytics/src/vue/Confetti.vue";n.default=r.exports},6:function(t,n,e){}}]);
+//# sourceMappingURL=styles-legacy.f4eb7fbc6d16958d6d70.js.map
