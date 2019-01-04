43c762dd21259e788b7fe73f0d39ce76676bbf9b
Andrew Welch <andrew@nystudio107.com>
2019-01-04 00:32:42 -0500

    Move to postcss-preset-env
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     plugins: [
-    	require('postcss-import'),
-    	require('postcss-extend'),
-    	require('postcss-simple-vars'),
-    	require('postcss-nested-ancestors'),
-    	require('postcss-nested'),
-    	require('postcss-hexrgba'),
-    	require('autoprefixer'),
-        require('tailwindcss')('./tailwind.config.js')
+        require("postcss-import"),
+        require('tailwindcss')('./tailwind.config.js'),
+        require('postcss-preset-env'),
     ]
-}
+};
