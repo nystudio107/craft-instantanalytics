c2dbdbec05fa13585e735152e4d872680b59379d
Andrew Welch <andrew@nystudio107.com>
2019-09-27 11:24:13 -0400

    Updated build assets
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/instantanalytics/dist/js/styles.js
+++ src/assetbundles/instantanalytics/dist/js/styles.js
@@ -2,9 +2,9 @@
  * @project        Instant Analytics
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Sat, Nov 17, 2018 10:41 PM ET
- * @release        6211d4623e3ac6b52396cbc3398e4d1f1ad184f7 [develop]
- * @copyright      Copyright (c) 2018 nystudio107
+ * @build          Fri, Sep 27, 2019 3:21 PM ET
+ * @release        c6d4c630545356560416d7b29aa1e4c766eae39c [develop]
+ * @copyright      Copyright (c) 2019 nystudio107
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{5:function(n,w,o){}}]);
