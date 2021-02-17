35bdc852e6b5ff9dd55a2e9c141eec1a2e4d7950
Andrew Welch <andrew@nystudio107.com>
2021-02-17 12:52:40 -0500

    Add includeJsModule & includeCssModule back in
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/variables/ManifestVariable.php
+++ src/variables/ManifestVariable.php
@@ -4,8 +4,12 @@ namespace nystudio107\instantanalytics\variables;
 
 use nystudio107\instantanalytics\helpers\Manifest as ManifestHelper;
 
+use craft\helpers\Template;
+
 use yii\base\InvalidConfigException;
 use yii\web\NotFoundHttpException;
+
+use Twig\Markup;
 
 class ManifestVariable
 {
@@ -34,5 +38,39 @@ class ManifestVariable
     public function registerCssModules(array $modules)
     {
         ManifestHelper::registerCssModules($modules);
+    }
+
+    /**
+     * Get the passed in JS module from the manifest, then output a `<script src="">` tag for it in the HTML
+     *
+     * @param string     $moduleName
+     * @param bool       $async
+     * @param null|array $config
+     *
+     * @return null|Markup
+     * @throws NotFoundHttpException
+     */
+    public function includeJsModule(string $moduleName, bool $async = false)
+    {
+        return Template::raw(
+            ManifestHelper::includeJsModule($moduleName, $async) ?? ''
+        );
+    }
+
+    /**
+     * Get the passed in CS module from the manifest, then output a `<link>` tag for it in the HTML
+     *
+     * @param string     $moduleName
+     * @param bool       $async
+     * @param null|array $config
+     *
+     * @return Markup
+     * @throws NotFoundHttpException
+     */
+    public function includeCssModule(string $moduleName, bool $async = false): Markup
+    {
+        return Template::raw(
+            ManifestHelper::includeCssModule($moduleName, $async) ?? ''
+        );
     }
 }
