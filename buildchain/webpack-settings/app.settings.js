0f085341fc773bfd41bf956f585e6c70d9443813
Andrew Welch <andrew@nystudio107.com>
2021-02-18 08:29:29 -0500

    Fix double slash
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- buildchain/webpack-settings/app.settings.js
+++ buildchain/webpack-settings/app.settings.js
@@ -7,7 +7,7 @@ const path = require('path');
 // settings
 module.exports = {
     alias: {
-        '@css': path.resolve('../src/assetbundles/instantanalytics/src//css'),
+        '@css': path.resolve('../src/assetbundles/instantanalytics/src/css'),
         '@img': path.resolve('../src/assetbundles/instantanalytics/src/img'),
         '@js': path.resolve('../src/assetbundles/instantanalytics/src/js'),
         '@vue': path.resolve('../src/assetbundles/instantanalytics/src/vue'),
