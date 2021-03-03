a918d0c9ebb0fc60001e9225a01f742a8c6a4352
Andrew Welch <andrew@keluli.local>
2021-03-03 15:09:14 -0500

    Updated assets build

--- src/assetbundles/instantanalytics/dist/js/welcome.js
+++ src/assetbundles/instantanalytics/dist/js/welcome.js
@@ -2,7 +2,7 @@
  * @project        instantanalytics
  * @name           welcome.js
  * @author         Andrew Welch
- * @build          Wed Mar 03 2021 01:34:44 GMT+0000 (Coordinated Universal Time)
+ * @build          Wed Mar 03 2021 20:07:02 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
