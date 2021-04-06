4504a42f558ddca8b884d82e045d783baf2a2488
Andrew Welch <andrew@nystudio107.com>
2021-04-05 23:32:49 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/instantanalytics/dist/js/instantanalytics.js
+++ src/assetbundles/instantanalytics/dist/js/instantanalytics.js
@@ -2,9 +2,9 @@
  * @project        instantanalytics
  * @name           instantanalytics.js
  * @author         Andrew Welch
- * @build          Wed Mar 03 2021 20:07:02 GMT+0000 (Coordinated Universal Time)
+ * @build          Tue Apr 06 2021 03:31:09 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
-(self.webpackChunkinstantanalytics=self.webpackChunkinstantanalytics||[]).push([[609],{715:function(n,s,t){"use strict";t.p}},0,[[715,666]]]);
+(self.webpackChunkinstantanalytics=self.webpackChunkinstantanalytics||[]).push([[609],{715:function(n,t,s){"use strict";s.p}},function(n){"use strict";n.O(0,[532],(function(){return t=715,n(n.s=t);var t}));n.O()}]);
 //# sourceMappingURL=instantanalytics.js.map
