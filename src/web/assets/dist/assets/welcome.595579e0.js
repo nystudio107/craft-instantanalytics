fa5ed935b3a6ef39e2b11dd63b8d4aa4444572ad
Andrew Welch <andrew@nystudio107.com>
2022-01-06 00:28:29 -0500

    chore: Updated assets build

---
+++ src/web/assets/dist/assets/welcome.595579e0.js
@@ -0,0 +1,2 @@
+import{d as n,r,C as c,o as s,a,c as l,b as p,e as u}from"./vendor.d81cffb0.js";const i=n({setup(o){const e={defaultType:"rect",defaultSize:15,defaultColors:["DodgerBlue","OliveDrab","Gold","pink","SlateBlue","lightblue","Violet","PaleGreen","SteelBlue","SandyBrown","Chocolate","Crimson"]},t=r(new c);return s(()=>{t.value.start(e),setTimeout(()=>{t.value.stop()},5e3)}),(_,m)=>(a(),l("div"))}}),f=n({setup(o){return(e,t)=>(a(),p(i))}}),d=async()=>u(f).mount("#app-container");d().then(()=>{console.log()});
+//# sourceMappingURL=welcome.595579e0.js.map
