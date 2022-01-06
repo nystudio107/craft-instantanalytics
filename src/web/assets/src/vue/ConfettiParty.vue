12153f6ed1cab47ce9b697f8545cd36e5a038bcf
Andrew Welch <andrew@nystudio107.com>
2022-01-06 00:27:48 -0500

    refactor: Use <script setup>

---
+++ src/web/assets/src/vue/ConfettiParty.vue
@@ -0,0 +1,23 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Confetti from 'vue-confetti/src/confetti.js';
+
+const config: Partial<ConfettiConfig> = {
+  defaultType: 'rect',
+  defaultSize: 15,
+  defaultColors: ['DodgerBlue', 'OliveDrab', 'Gold', 'pink', 'SlateBlue', 'lightblue', 'Violet', 'PaleGreen', 'SteelBlue', 'SandyBrown', 'Chocolate', 'Crimson'],
+};
+
+const confetti = ref(new Confetti());
+
+onMounted(() => {
+  confetti.value.start(config);
+  setTimeout(() => {
+    confetti.value.stop();
+  }, 5000);
+});
+</script>
